8d913a82fe25fae979bf9bbb8b9eb5bc159a1ca6
Ashton Hefley <ajdhefley@gmail.com>
2022-06-29 19:04:05 -0500

    Update NestJS names and config values

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { WinstonLoggerService } from './infrastructure/logging/winston-logger.service';
 
-async function bootstrap() {
+(async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule, {
     logger: new WinstonLoggerService(),
     cors: true
   });
-  app.enableCors();
-  await app.listen(4040);
-}
-bootstrap();
+
+  await app.enableCors();
+  await app.listen(process.env.PORT);
+})();
