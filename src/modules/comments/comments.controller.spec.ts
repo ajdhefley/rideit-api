03eb66d34b2e582340dd4d01feae089ffe1613a6
Ashton Hefley <ajdhefley@gmail.com>
2022-06-30 22:35:11 -0500

    Add comment controller spec

---
+++ src/modules/comments/comments.controller.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CommentsController } from './comments.controller';
+import { CommentService } from './services/comment.service';
+
+describe('CommentsController', () => {
+  let commentsController: CommentsController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [CommentsController],
+      providers: [CommentService],
+    }).compile();
+
+    commentsController = app.get<CommentsController>(CommentsController);
+  });
+});
