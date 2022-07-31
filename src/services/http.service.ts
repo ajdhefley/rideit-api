0fc3b867711f5adddf3c45dfeb54028705adddb5
Ashton Hefley <ajdhefley@gmail.com>
2022-07-31 13:05:29 -0500

    Log axios errors

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -7,7 +7,7 @@ export class HttpService {
         return new Promise((resolve, reject) => {
             axios.get<T>(url)
                 .then((result) => resolve(result.data))
-                .catch(reject);
+                .catch((err) => this.catchError(err, reject));
         });
     }
 
@@ -15,7 +15,7 @@ export class HttpService {
         return new Promise((resolve, reject) => {
             axios.put(url)
                 .then(resolve)
-                .catch(reject);
+                .catch((err) => this.catchError(err, reject));
         });
     }
 
@@ -23,7 +23,7 @@ export class HttpService {
         return new Promise((resolve, reject) => {
             axios.post(url, body)
                 .then(resolve)
-                .catch(reject);
+                .catch((err) => this.catchError(err, reject));
         });
     }
 
@@ -31,7 +31,7 @@ export class HttpService {
         return new Promise((resolve, reject) => {
             axios.patch(url)
                 .then(resolve)
-                .catch(reject);
+                .catch((err) => this.catchError(err, reject));
         });
     }
 
@@ -39,7 +39,24 @@ export class HttpService {
         return new Promise((resolve, reject) => {
             axios.delete(url)
                 .then(resolve)
-                .catch(reject);
+                .catch((err) => this.catchError(err, reject));
         });
+    }
+
+    private catchError(error: any, onRejected: (reason: any) => void) {
+        if (error.response) {
+            // Request made and server responded
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+
+        onRejected(error);
     }
 }
