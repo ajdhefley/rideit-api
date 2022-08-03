714fb66ba6be6800093e27134732d7cfdc80756b
Ashton Hefley <ajdhefley@gmail.com>
2022-08-02 21:34:31 -0500

    Remove CORS now that nginx is set up to share host with client

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,6 @@ import { AppModule } from './modules/app.module';
     });
 
     await app.use(cookieParser());
-    await app.enableCors({
-        credentials: true,
-        origin: process.env.CLIENT_URI
-    });
-    // TODO: use reverse proxy for same port and no cross-origin
 
     app.listen(process.env.PORT || 4040, process.env.HOST, () => {
         Logger.log(`Server running on PORT ${process.env.PORT || 4040}`);
