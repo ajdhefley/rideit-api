5e4e9c7a896adc7eb663ff14d5f5ae64978e1036
Ashton Hefley <ajdhefley@gmail.com>
2022-08-02 21:33:40 -0500

    Run all pages by default for data_setup

--- entrypoint.tools.sh
+++ entrypoint.tools.sh
@@ -1,3 +1,3 @@
-npm run tools:scraper -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --pages=1,2,3 --reset
+npm run tools:scraper -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --reset
 npm run tools:encoder -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker
-npm run tools:mock -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --reviews=10 --comments=0 --reset
+npm run tools:mock -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --reviews=4 --comments=2 --reset
