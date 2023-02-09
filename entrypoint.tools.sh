f60f80ea6d2c8cbb84950c9c7e1b6233811a4ab5
Ashton Hefley <ajdhefley@gmail.com>
2023-02-08 19:52:48 -0600

    Update tools entrypoint (batched run commands)

--- entrypoint.tools.sh
+++ entrypoint.tools.sh
@@ -1,4 +1,4 @@
-npm run tools:scraper -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --reset
-npm run tools:encoder -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker
+npm run tools:scraper -- --api-host=http://host.docker.internal:5000/api --reset
+npm run tools:encoder -- --api-host=http://host.docker.internal:5000/api
 npm run tools:mock -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker --reviews=4 --comments=2 --reset
 npm run tools:rank -- --db-host=host.docker.internal --db-user=postgres --db-password=postgres --db-name=coasterranker
